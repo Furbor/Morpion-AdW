44fad766f7e89698f4f93ba9cf7700b6c5f8a394
Furbor <45755076+Furbor@users.noreply.github.com>
2019-05-09 21:14:45 +0200

    Add files via upload

---
+++ morpion.php
@@ -0,0 +1,67 @@
+<?php
+
+    if (!isset($_GET['taille'])) {
+        header('Location: index.html');
+        exit();
+    }
+
+	function morpion($n) {
+		$table = "	<tr>\n";
+		for($k=1; $k<$n*$n+1; $k++) {
+			$table.="				<td class='carre' id='case".$k."' onclick=play(case".$k.") played='notPlayed'></td>\n";
+			if ($k%$n == 0 && $k != $n*$n) {
+				$table.= "			</tr>\n";
+				$table.="			<tr>\n";
+			}
+		}
+		$table.="		   </tr>\n";
+		return $table;
+	}
+
+?>
+<!DOCTYPE html>
+<html lang='fr'>
+<head>
+    <meta charset="utf-8">
+    <title>Morpion - Jeu</title>
+    <link rel="shortcut icon" type="image/png" href="resources/favicon.png"/>
+    <?php 
+
+    if (($_GET['taille'] == 3 || $_GET['taille'] == 5)) {
+        if (isset($_GET['darkmode'])) {
+    	   echo "<link rel='stylesheet' type='text/css' href='css/taille".$_GET['taille']."_darkmode.css'>\n";
+        }
+        else {
+           echo "<link rel='stylesheet' type='text/css' href='css/taille".$_GET['taille'].".css'>\n";           
+        }
+    }
+    else {
+        header('Location: index.html');
+        exit();
+    }
+
+    ?>
+    <script type="text/javascript" src="js/morpion.js"></script>
+</head>
+<body>
+    <div id="audio">
+        <audio controls autoplay loop="auto">
+            <source src="resources/play_theme.mp3"> 
+        </audio>
+    </div>
+    <div id="titre">
+        Morpion <?php echo $_GET['taille']."x".$_GET['taille']; ?>
+    </div>
+	<div class="jeu">
+    <table id="center">
+        <tbody>
+        <?php
+        
+        echo morpion($_GET['taille']);
+
+        ?>
+        </tbody>
+    </table>
+</div>
+</body>
+</html>
